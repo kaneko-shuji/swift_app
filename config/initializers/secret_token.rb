bed58b1ba94687e7700256093e0e21c8103d7005
kaneko-shuji <kaneko-shuji@umin.ac.jp>
2015-07-29 13:35:19 +0900

    Initital repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SwiftApp::Application.config.secret_token = '8d29bd18121e43283b299b344cda637d5ff37b1014d5688584177314fec6446edc2b7f6925f0ac4087ab45ac416cfa905c0ebaf1a41750656edfe8b3c0b310e1'
